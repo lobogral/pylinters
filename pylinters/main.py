338b2fc33b6433ee27f5e69ec4a7fc4edeae0caf
Camilo Sabogal <lobogral@gmail.com>
2021-11-01 20:31:42 -0500

    Creo funcion y corrigo ortografia

--- pylinters/main.py
+++ pylinters/main.py
@@ -1,41 +1,38 @@
-"""Utiliza varios linters para analizar codigo."""
+"""Utiliza varios linters para analizar código."""
 import sys
 import os
 from colorama import Fore
 from colorama import init
 
 
+def colorear_titulo(titulo: str) -> None:
+    """Colorea e imprime el título."""
+    print(Fore.CYAN, end="")
+    print(titulo)
+    print(Fore.RESET, end="")
+
+
 def main():
-    """Metodo principal."""
+    """Método principal."""
     init()
 
-    print(Fore.CYAN, end="")
-    print("flake8")
-    print(Fore.RESET, end="")
+    colorear_titulo("flake8")
     os.system('flake8 ' + sys.argv[-1])
 
-    print(Fore.CYAN, end="")
-    print("mypy")
-    print(Fore.RESET, end="")
+    colorear_titulo("mypy")
     os.system('mypy --ignore-missing-imports ' + sys.argv[-1])
 
     if sys.argv[1] == "--no-docs":
 
-        print(Fore.CYAN, end="")
-        print("pylint")
-        print(Fore.RESET, end="")
+        colorear_titulo("pylint")
         os.system('pylint --disable=missing-docstring ' + sys.argv[-1])
 
     else:
 
-        print(Fore.CYAN, end="")
-        print("pylint")
-        print(Fore.RESET, end="")
+        colorear_titulo("pylint")
         os.system('pylint ' + sys.argv[-1])
 
-        print(Fore.CYAN, end="")
-        print("pydocstyle")
-        print(Fore.RESET, end="")
+        colorear_titulo("pydocstyle")
         os.system('pydocstyle --convention=numpy ' + sys.argv[-1])
 
 
